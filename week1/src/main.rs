4939f13a8bf8d0ec7e7889184419f373d9089d00
Geo. Naumov <george.naumov@outlook.com>
2024-11-24 04:49:38 +0200

    Added some example code

--- week1/src/main.rs
+++ week1/src/main.rs
@@ -1,3 +1,38 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
 fn main() {
     println!("Hello, world!");
+
+    let fruits = vec!["Apple", "Banana", "Cherry"];
+    for fruit in &fruits {
+        println!("{}", fruit);
+    }
+
+    let mut map = std::collections::HashMap::new();
+    map.insert("apple", 10);
+    map.insert("banana", 8);
+    map.insert("orange", 15);
+    for (key, value) in &map {
+    println!("{}: {}", key, value);
+    }
+
+    let mut fruit_salad: Vec<&str> = vec![
+        "Apple",
+        "Banana",
+        "Cherry",
+        "Peach",
+        "Pear",
+    ];
+
+    let mut rng = thread_rng();
+    fruit_salad.shuffle(&mut rng);
+
+    for (i, item) in fruit_salad.iter().enumerate() {
+        if i != fruit_salad.len() - 1 {
+            println!("{} ", item);
+        } else {
+            println!("{}", item);
+        }
+    }
 }
